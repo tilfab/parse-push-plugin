64fa6aca5438a29e676fec1cf128a71563c18e5b
Lars Lernestål <lars@tilf.se>
2015-08-26 16:50:44 +0200

    fix js source

--- www/parse-push-plugin.js
+++ www/parse-push-plugin.js
@@ -1,32 +1,29 @@
-cordova.define("com.phonegap.parsepushplugin.ParsePushPlugin", function(require, exports, module) {
-    var serviceName = 'ParsePushPlugin';
+var serviceName = 'ParsePushPlugin';
 
-    var ParsePushPlugin = {
-        getInstallationId: function(successCb, errorCb) {
-            cordova.exec(successCb, errorCb, serviceName, 'getInstallationId', []);
-        },
+var ParsePushPlugin = {
+    getInstallationId: function(successCb, errorCb) {
+        cordova.exec(successCb, errorCb, serviceName, 'getInstallationId', []);
+    },
 
-        getInstallationObjectId: function(successCb, errorCb) {
-            cordova.exec(successCb, errorCb, serviceName, 'getInstallationObjectId', []);
-        },
+    getInstallationObjectId: function(successCb, errorCb) {
+        cordova.exec(successCb, errorCb, serviceName, 'getInstallationObjectId', []);
+    },
 
-        getSubscriptions: function(successCb, errorCb) {
-            cordova.exec(successCb, errorCb, serviceName, 'getSubscriptions',[]);
-        },
+    getSubscriptions: function(successCb, errorCb) {
+        cordova.exec(successCb, errorCb, serviceName, 'getSubscriptions',[]);
+    },
 
-        subscribe: function(channel, successCb, errorCb) {
-            cordova.exec(successCb, errorCb, serviceName, 'subscribe', [ channel ]);
-        },
+    subscribe: function(channel, successCb, errorCb) {
+        cordova.exec(successCb, errorCb, serviceName, 'subscribe', [ channel ]);
+    },
 
-        unsubscribe: function(channel, successCb, errorCb) {
-            cordova.exec(successCb, errorCb, serviceName, 'unsubscribe', [ channel ]);
-        },
+    unsubscribe: function(channel, successCb, errorCb) {
+        cordova.exec(successCb, errorCb, serviceName, 'unsubscribe', [ channel ]);
+    },
 
-        unsubscribeFromAll: function (successCb, errorCb) {
-            cordova.exec(successCb, errorCb, serviceName, 'unsubscribeFromAll', []);
-        }
-    };
+    unsubscribeFromAll: function (successCb, errorCb) {
+        cordova.exec(successCb, errorCb, serviceName, 'unsubscribeFromAll', []);
+    }
+};
 
-    module.exports = ParsePushPlugin;
-
-});
+module.exports = ParsePushPlugin;
